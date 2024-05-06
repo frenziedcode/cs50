505e3285b4eb0cf1814e5d9ca8e3786f92a06832
frenziedcode <contact.viniciuslopes@gmail.com>
2024-05-06 14:39:59 +0100

    Readabillity

--- Week 2/Problem Set 2/readability/readability.c
+++ Week 2/Problem Set 2/readability/readability.c
@@ -1,91 +1,85 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
-
-float calcReadability(float word, float phrase);
+#include <string.h>
 
 int count_letters(string text);
 int count_words(string text);
-int count_sentences(string text);
+int count_sentenses(string text);
 
 int main(void)
 {
-    string phrase = get_string("Text: ");
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentenses = count_sentenses(text);
 
     /*
-    printf("%i letters\n", count_letters(phrase));
-    printf("%i words\n", count_words(phrase));
-    printf("%i sentences\n", count_sentences(phrase));
+    printf("Letters: %i \n", letters);
+    printf("Words: %i \n", words);
+    printf("Sentenses: %i \n", sentenses);
     */
 
-    float countLetter = count_letters(phrase);
-    float countWord = count_words(phrase);
-    float countPhrase = count_sentences(phrase);
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentenses / (float) words * 100;
 
-    // Calc avarage
-    float avarageWord = (float) countLetter / (float) countWord * 100.0;
-    float avaragePhrase = (float) countPhrase / (float) countWord * 100.0;
-    float final_index = calcReadability(avarageWord, avaragePhrase);
-    /*
-    printf("Avarage Word: %.2f\n", avarageWord);
-    printf("Avarage Phrase: %.2f\n", avaragePhrase);
-    printf("\n");
-    */
-    if (final_index < 16 && final_index > 1)
-        printf("Grade %.0f\n", final_index);
-    else if (final_index <= 1)
-        printf("Before Grade 1\n");
-    else
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index >= 16)
+    {
         printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 
     return 0;
 }
 
-float calcReadability(float word, float phrase)
-{
-    float index = 0.0588 * word - 0.296 * phrase - 15.8;
-
-    return index;
-}
-
 int count_letters(string text)
 {
-    int countLetter = 0;
-    int i = 0;
-    while (text[i] != 0)
+    int letters = 0;
+    for (int i = 0; i < strlen(text); i++)
     {
-        if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
-            countLetter++;
-
-        i++;
+        if ((text[i] > 64 && text[i] < 91) || (text[i] > 96 && text[i] < 123))
+        {
+            letters++;
+        }
     }
 
-    return countLetter;
+    return letters;
 }
 
 int count_words(string text)
 {
-    int countWord = 1;
-    int i = 0;
-    while (text[i] != 0)
+    int words = 1;
+    for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] == 32 && text[i + 1] != 32)
-            countWord++;
-        i++;
+        if (text[i] == ' ')
+        {
+            words++;
+        }
     }
 
-    return countWord;
+    return words;
 }
 
-int count_sentences(string text)
+int count_sentenses(string text)
 {
-    int countPhrase = 0;
-    int i = 0;
-    while (text[i] != 0)
+    int sentenses = 0;
+    for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] == 46 || text[i] == 33 || text[i] == 63)
-            countPhrase++;
-        i++;
+        if ((text[i] == '.') || (text[i] == '!') || (text[i] == '?'))
+        {
+            sentenses++;
+        }
     }
 
-    return countPhrase;
+    return sentenses;
 }
